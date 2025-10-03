dd12442f750f9332686e03b840b2344e3f86059d
Gaptixa <gaptixdev@gmail.com>
2025-10-03 23:31:32 +0700

    solved

--- codeforces/800/1328A-Divisibility Problem.cpp
+++ codeforces/800/1328A-Divisibility Problem.cpp
@@ -5,11 +5,14 @@ using namespace std;
 
 int main () {
     
-    ll a,b; cin >> a >> b;
-    if (a % b == 0) {
-        cout << 0 << endl;
-    }else{
-        cout << b - (a % b) << endl;
+    int t; cin >> t;
+    while (t--) {
+        ll a,b; cin >> a >> b;
+        if (a % b == 0) {
+            cout << 0 << endl;
+        }else{
+            cout << b - (a % b) << endl;
+        }
     }
 
 }
