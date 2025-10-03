4ca54727656f104ef0441c1195907e4d1a7b40b4
Gaptixa <gaptixdev@gmail.com>
2025-10-03 07:34:19 +0700

    solved

---
+++ codeforces/800/1328A-Divisibility Problem.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main () {
+    
+    ll a,b; cin >> a >> b;
+    if (a % b == 0) {
+        cout << 0 << endl;
+    }else{
+        cout << b - (a % b) << endl;
+    }
+
+}
