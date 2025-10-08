2d8b39afea7c90ef25b81405e434331bcecbbb9e
Gaptixa <gaptixdev@gmail.com>
2025-10-08 13:28:06 +0700

    solved

--- codeforces/1000/2148D.cpp
+++ codeforces/1000/2148D.cpp
@@ -1,17 +1,22 @@
 #include <bits/stdc++.h>
+#define ll long long
 
 using namespace std;
 
 int main () {
 
-    int n,ans=0; cin >> n;
-    vector<int> genap, ganjil;
+    int n; cin >> n;
 
     while (n--) {
-        int a; cin >> a;
-        for (int i = 0; i < a; i++) {
-            int x; cin >> x;
-            (x % 2 == 0) ? genap.push_back(x) : ganjil.push_back(x);
+        ll a,ans=0; cin >> a;
+        vector<ll> genap, ganjil;
+        for (ll i = 0; i < a; i++) {
+            ll x; cin >> x;
+            if (x % 2 == 0) {
+                genap.push_back(x);
+            } else {
+                ganjil.push_back(x);
+            }
         }
 
         if (ganjil.size() == 0) {
@@ -19,16 +24,17 @@ int main () {
             continue;
         }
 
-        sort(genap.begin(), genap.end());
-        sort(ganjil.begin(), ganjil.end(), greater<int>());
-        for (int b : genap) {
-            ans += b;
-        }
+        sort(ganjil.begin(), ganjil.end(), greater<ll>());
 
-        for (int i = 0; i < ganjil.size(); i++) {
-            if (i % 2 == 1) {
-                ans += ganjil[i];
+        int cnt = 0;
+        for (ll i = 0; i < ganjil.size(); i++) {
+            if (i % 2 == 0) {
+                ans += ganjil[cnt];
+                cnt++;
             }
+        }
+        for (ll b : genap) {
+            ans += b;
         }
 
         cout << ans << endl;
