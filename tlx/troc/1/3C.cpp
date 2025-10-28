aa9fdf597a31c779c3a8adadfe3f51ee452f1dd3
Gaptixa <gaptixdev@gmail.com>
2025-10-28 19:28:58 +0700

    troc-1-c ongoing

---
+++ tlx/troc/1/3C.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll long long;
+
+using namespace std;
+
+int main () {
+
+    int n,q;
+    cin >> n >> q;
+    vector<int> arr(n);
+
+    for(auto &a : arr) a = 0;
+
+    for (int i = 0; i < q; i++) {
+        int t,x,y; cin >> t >> x >> y;
+        if(t % 2 == 0){
+            for () {
+                
+            }
+        }else{
+
+        }
+    }
+
+}
