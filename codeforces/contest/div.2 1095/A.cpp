8005b8701f1b18229740e89fd6f3685c7d115e1e
Gaptixa <gaptixdev@gmail.com>
2026-04-29 23:01:29 +0700

    bismillah gemastik

---
+++ codeforces/contest/div.2 1095/A.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ██████╗░██╗░██████╗███╗░░░███╗██╗██╗░░░░░██╗░░░░░░█████╗░██╗░░██╗
+// ██╔══██╗██║██╔════╝████╗░████║██║██║░░░░░██║░░░░░██╔══██╗██║░░██║
+// ██████╦╝██║╚█████╗░██╔████╔██║██║██║░░░░░██║░░░░░███████║███████║
+// ██╔══██╗██║░╚═══██╗██║╚██╔╝██║██║██║░░░░░██║░░░░░██╔══██║██╔══██║
+// ██████╦╝██║██████╔╝██║░╚═╝░██║██║███████╗███████╗██║░░██║██║░░██║
+// ╚═════╝░╚═╝╚═════╝░╚═╝░░░░░╚═╝╚═╝╚══════╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝
+
+#define bismillah ios::sync_with_stdio(false); cin.tie(NULL);
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define fr(i, n) for (ll i = 0; i < (n); i++)
+#define frr(i,n) for (ll i = 1; i <= (n); i++)
+#define pb push_back
+#define fi first
+#define se second
+#define no cout << "NO" << endl;
+#define yes cout << "YES" << endl;
+#define maxx(a,b,c) max((a), max((b),(c))
+#define minn(a,b,c) min((a), min((b),(c))
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+
+const ll INF = 1e18;
+const ll MOD = 676767677;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+ll fpb(ll a, ll b){
+    if(b == 0) return a;
+    return fpb(b, a % b);
+}
+
+ll kpk(ll a, ll b){
+    return a / fpb(a,b) * b;
+}
+
+void solve(){
+    
+    ll n; cin>>n;
+    vl arr(n);
+    fr(i,n)cin>>arr[i];
+
+    ll akhir=-1;
+    for(int i=n-1;i>=0;i--){
+        if(arr[i]>1){
+            akhir=i;   
+            break;
+        }
+    }
+
+    ll sum=0;
+    bool cek=false;
+    fr(i,n){
+        if(arr[i]>1){
+            sum = (sum+arr[i])%MOD;
+        }else{
+            if(akhir==-1||i>akhir){
+                cek=true;
+            }
+        }
+    }
+
+    if(cek)sum = (sum+1)%MOD;
+    cout<<sum<<endl;
+
+}
+
+int main(){
+    bismillah;
+
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
